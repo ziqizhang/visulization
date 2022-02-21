de06b4fcb3e943fbc39364c7cd45174c5afd65c0
zz <ziqizhang.email@gmail.com>
2022-02-21 16:02:19 +0000

    .

---
+++ boxplot/viz_boxplot.py
@@ -0,0 +1,37 @@
+import json
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import pandas as pd
+import sys
+
+
+labelsize = 16
+rcParams['xtick.labelsize'] = labelsize
+rcParams['ytick.labelsize'] = labelsize
+
+def plot(in_file,outfolder):
+    f = open(in_file)
+    json_data=json.load(f)
+    f.close()
+    for item in json_data:
+        k = next(iter(item))
+        data=item[k]
+        real = data['real']
+        real['fliers']=[]
+        fake = data['fake']
+        fake['fliers'] = []
+        stats = [real, fake]
+
+        fs = 12  # fontsize
+
+        fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 6), sharey=True)
+        axes.bxp(stats, positions=[1,1.3])
+        axes.set_title('Boxplot for {}'.format(k), fontsize=fs)
+        plt.savefig(outfolder+"/"+k+".png", format='png', dpi=300)
+        plt.clf()
+
+if __name__ == "__main__":
+    infile="/home/zz/Work/data/amazon/labelled/small/stats_descriptive_rating.json"
+    outfolder="/home/zz/Work/data/amazon/labelled/stats/boxplots"
+    plot(infile, outfolder)
---
+++ heatmap/viz_heatmap.py
@@ -0,0 +1,42 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import pandas as pd
+import seaborn as sns
+from matplotlib import rcParams
+
+labelsize = 3
+rcParams['xtick.labelsize'] = labelsize
+rcParams['ytick.labelsize'] = labelsize
+
+def plot(in_file, out_file, rotate_y=0, rotate_x=0):
+    df = pd.read_csv(in_file, header=0)
+    row_header=df.columns.values[1:]
+    df=df.values
+    col_header=df[:,0]
+    df=df[:, 1:]
+    df = df.astype(np.float)
+
+    ax = sns.heatmap(df, linewidth=0.5)
+    y_labels=list(col_header)
+    x_labels = list(row_header)
+    ax.set_yticklabels(y_labels)
+    ax.set_xticklabels(x_labels)
+    plt.yticks(rotation=rotate_y)
+    plt.xticks(rotation=rotate_x)
+    plt.subplots_adjust(left=0.4)
+    plt.subplots_adjust(bottom=0.2)
+    plt.savefig(out_file, format='png', dpi=300)
+    plt.clf()
+    print("done")
+
+
+if __name__ == "__main__":
+    infile = "/home/zz/Work/data/amazon/labelled/stats/stats_dist_rating.csv"
+    outfile = "/home/zz/Work/data/amazon/labelled/stats/heatmap_rating.png"
+    plot(infile, outfile,0,0)
+
+    infile = "/home/zz/Work/data/amazon/labelled/stats/stats_dist_words.csv"
+    outfile = "/home/zz/Work/data/amazon/labelled/stats/heatmap_length.png"
+    plot(infile, outfile,0,90)
